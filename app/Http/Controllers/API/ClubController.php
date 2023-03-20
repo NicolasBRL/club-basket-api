0e164ca288a792b9bc6b61e0eaa1e34fcf6d1e23
Nicolas BRAILLON <nicolas.braillonpro@gmail.com>
2023-03-20 10:03:34 +0100

    Ajout méthode controlleur club

--- app/Http/Controllers/API/ClubController.php
+++ app/Http/Controllers/API/ClubController.php
@@ -5,46 +5,55 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Club;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ClubController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $clubs = DB::table('clubs')
+            ->get()
+            ->toArray();
+        return response()->json([
+            'status' => 'Success', 'data' => $clubs,
+        ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'nameClub' => 'required|max:100',
+        ]);
+        $club = Club::create([
+            'nameClub' => $request->nameClub,
+        ]);
+        return response()->json([
+            'status' => 'Success', 'data' => $club,
+        ]);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Club $club)
     {
-        //
+        return response()->json($club);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Club $club)
     {
-        //
+        $this->validate($request, [
+            'nameClub' => 'required|max:100',
+        ]);
+
+        $club->update([
+            'nameClub' => $request->nameClub,
+        ]);
+        return response()->json([
+            'status' => 'Mise à jour avec succèss'
+        ]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Club $club)
     {
-        //
+        return response()->json([
+            'status' => 'Supprimer avec succès']);
     }
 }

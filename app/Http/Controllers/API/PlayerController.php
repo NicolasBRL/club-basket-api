554bf3eb5050465898aa395471165fb522d6559e
Nicolas BRAILLON <nicolas.braillonpro@gmail.com>
2023-03-20 09:58:31 +0100

    Création des méthodes du contrôleur

--- app/Http/Controllers/API/PlayerController.php
+++ app/Http/Controllers/API/PlayerController.php
@@ -5,46 +5,65 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Player;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PlayerController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        // On récupère tous les joueurs
+        $players = DB::table('players')->get()
+            ->toArray();
+        // On retourne les informations des utilisateurs en JSON
+        return response()->json([
+            'status' => 'Success', 'data' => $players,
+        ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'firstName' => 'required|max:100', 'lastName' => 'required|max:100', 'height' => 'required|max:100', 'position' => 'required|max:100',
+        ]);
+        // On crée un nouvel utilisateur
+        $player = Player::create([
+            'firstName' => $request->firstName, 'lastName' => $request->lastName, 'height' => $request->height, 'position' => $request->position,
+        ]);
+        // On retourne les informations du nouvel utilisateur en JSON
+        return response()->json([
+            'status' => 'Success', 'data' => $player,
+        ]);
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(Player $player)
     {
-        //
+        return response()->json($player);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Player $player)
     {
-        //
+        $this->validate($request, [
+            'firstname' => 'required|max:100',
+            'lastname' => 'required|max:100', 'height' => 'required|max:100', 'position' => 'required|max:100',
+        ]);
+        // On crée un nouvel utilisateur
+        $player->update([
+            'firstname' => $request->firstname, 'lastname' => $request->lastname, 'height' => $request->height, 'lastname' => $request->lastname, 'position' => $request->position,
+        ]);
+        // On retourne les informations du nouvel utilisateur en JSON
+        return response()->json([
+            'status' => 'Mise à jour avec succèss'
+        ]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Player $player)
     {
-        //
+        // On supprime l'utilisateur
+        $player->delete();
+        // On retourne la réponse JSON
+        return response()->json([
+            'status' => 'Supprimer avec succès avec succèss'
+        ]);
     }
 }

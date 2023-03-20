0b64dba02c73dc24e206630c470c4853ed569f2e
Nicolas BRAILLON <nicolas.braillonpro@gmail.com>
2023-03-20 10:01:26 +0100

    Création migration/controller/model pour l'entité Club

---
+++ app/Http/Controllers/API/ClubController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Club;
+use Illuminate\Http\Request;
+
+class ClubController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Club $club)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Club $club)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Club $club)
+    {
+        //
+    }
+}
